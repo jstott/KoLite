cd4b5be6de5ee05bbd1f0083ecae139d29e17287
jstott <jwstott@gmail.com>
2013-01-11 16:40:39 -0500

    another round of testing...

--- knockout.pagedObservableArray.js
+++ knockout.pagedObservableArray.js
@@ -1,12 +1,9 @@
 /*global ko: false*/
-// By: Hans James Stott
+
+// By: James Stott
 // https://github.com/jstott/KoLite.git
 //
 // Knockout.pagedObservableArray
-//
-// John Papa 
-//          http://johnpapa.net
-//          http://twitter.com/@john_papa
 //
 // Depends on scripts:
 //			jquery
@@ -18,13 +15,12 @@
 //
 //  Usage:      
 //          To Setup Tracking, add this tracker property to your viewModel    
-//              ===> pagedRosters = ko.pagedObservableArray({
+//              ===> pagedRosters = new ko.pagedObservableArray({
 //            serverPaging: true,
-//            pageSize: 5,
+//            pageSize: 10,
+//            aggregateResults: false, //combine each page request locally
 //            loadPage: function(options) { // options will contain contents of parameterMap or pageSize/pageIndex
-//                var def = $.Deferred();
-//                model.Roster.datacontext.getRosterClasses(df, options);
-//                return df;
+//                return viewModel.getRosterClasses(options);
 //            },
 //           parameterMap: function(options) {
 //                var parameters = {
@@ -54,17 +50,21 @@
         // AMD anonymous module with hard-coded dependency on "knockout"
         define(["knockout", "exports"], factory);
     } else {
-        // <script> tag: use the global `ko` object, attaching a `mapping` property
-        factory(ko, ko.mapping = {});
+        factory(ko, {});
     }
 }(function (ko, exports) {
     "use strict";
 
     ko.pagedObservableArray = function (options) {
+
         if (!options) { throw "Options not specified"; }
         if (!options.loadPage) { throw "loadPage not specified on options"; }
 
+        options.schema = options.schema || { data: 'Data', count: 'Count' };
+
         var
+            self = this,
+
 		//the complete data collection
 	     _allData = ko.observableArray(options.data || []),
 
@@ -76,72 +76,96 @@
 
 		//the total count
 		_totalCount = ko.observable(0),
-		
+
 		//the number of pages
 		_pageCount = ko.computed(function () {
-			if (_totalCount() > 0)
-				return Math.ceil(_totalCount() / _pageSize()) || 1;
-			else
-				return Math.ceil(_allData().length / _pageSize()) || 1;
+		    if (_totalCount() > 0)
+		        return Math.ceil(_totalCount() / _pageSize()) || 1;
+		    else
+		        return Math.ceil(_allData().length / _pageSize()) || 1;
 		}),
-			
+
 		//the current page data
 		_page = ko.computed(function () {
-			var pageSize = _pageSize(),
+		    var pageSize = _pageSize(),
 				pageIndex = _pageIndex(),
 				startIndex = pageSize * pageIndex,
 				endIndex = pageSize * (pageIndex + 1);
+		    return _allData().slice(startIndex, endIndex);
+		}, this).extend({ throttle: 100 }),
 
-			return _allData().slice(startIndex, endIndex);
-		}, this),
+        // option to externally handle mapping of remote data
+        _map = options.map,
+
+        _loading = ko.observable(false),
 
 		//load a page of data, then display it
-		_loadPage = function() {
-			var dfdCallback,
-				paramOptions = {pageSize: _pageSize(), pageIndex: pageIndex()};
-			if (options.parameterMap)
-				paramOptions = options.parameterMap(paramOptions);
-			dfdCallback = options.loadPage(paramOptions);
-			$.when(dfdCallback).then(function (data, status) {
-                    if (status === 'success') {
-                        console.dir(data);
-                        console.log(status);
-                    }
-                });
+		_loadPage = function () {
+		    var
+		        deferred,
+				paramOptions = { pageSize: _pageSize(), pageIndex: _pageIndex() };
+		    _loading(true);
+		    if (options.parameterMap)
+		        paramOptions = options.parameterMap(paramOptions); // let consumer option to modify params
+
+		    deferred = options.loadPage(paramOptions);
+		    $.when(deferred).then(function (data, status) {
+		        var tmpArray = options.aggregateResults ? ko.utils.unwrapObservable(_allData()) || [] : [];
+		        if (status === 'success') {
+		            _totalCount(data[options.schema.count]); // capture count of items
+		            if (options.map)
+		                tmpArray = options.map(data[options.schema.data]);
+		            else
+		                tmpArray.push.apply(tmpArray, data[options.schema.data]);
+		            _allData(tmpArray); //  push items into our array
+		            if (!isNaN(paramOptions.pageIndex))
+		                _pageIndex(paramOptions.pageIndex);
+		        }
+		    }).always(function () {
+		        _loading(false);
+		    });
 		},
-		
-		
+
+        _refresh = function () {
+            _loadPage();
+        },
+
 		//move to the next page
-	        _nextPage = function () {
-	            if (_pageIndex() < (_pageCount() - 1)) {
-	                _pageIndex(_pageIndex() + 1);
-					_loadPage();
-	            }
-	        },
+	    _nextPage = function () {
+	        if (_pageIndex() < (_pageCount() - 1)) {
+	            _pageIndex(_pageIndex() + 1);
+	            _loadPage();
+	        }
+	    },
 
 		//move to the previous page
-	        _previousPage = function () {
-	            if (_pageIndex() > 0) {
-	                _pageIndex(_pageIndex() - 1);
-					_loadPage();
-	            }
-	        };
+	    _previousPage = function () {
+	        if (_pageIndex() > 0) {
+	            _pageIndex(_pageIndex() - 1);
+	            _loadPage();
+	        }
+	    };
 
         //reset page index when page size changes
-        _pageSize.subscribe(function () { _pageIndex(0); });
-        _allData.subscribe(function () { _pageIndex(0); });
+        _pageSize.subscribe(function () {
+            _pageIndex(0);
+            _loadPage();
+        });
+        //_allData.subscribe(function () { _pageIndex(0); });
 
-		if (options.autoLoad)
-			_loadPage();
-			
+
+        if (options.autoLoad)
+            _loadPage();
+
         //public members
-        this.allData = _allData;
-        this.pageSize = _pageSize;
-        this.pageIndex = _pageIndex;
-        this.page = _page;
-        this.pageCount = _pageCount;
-        this.nextPage = _nextPage;
-        this.previousPage = _previousPage;
-		this.totalCount = _totalCount;
+        self.allData = _allData;
+        self.pageSize = _pageSize;
+        self.pageIndex = _pageIndex;
+        self.page = _page;
+        self.pageCount = _pageCount;
+        self.nextPage = _nextPage;
+        self.previousPage = _previousPage;
+        self.refresh = _refresh;
+        self.totalCount = _totalCount;
     };
 }));
